ba358a76ecc0331ce66672497d686be4da47d3fe
mmillson <mgm3746@users.noreply.github.com>
2023-02-18 08:18:38 -0500

    Prepare for next release.

--- src/main/java/org/github/joa/JvmOptions.java
+++ src/main/java/org/github/joa/JvmOptions.java
@@ -3265,14 +3265,6 @@ public class JvmOptions {
                 analysis.add(Analysis.ERROR_PARALLEL_SCAVENGE_PARALLEL_SERIAL_OLD);
             } else if (useParallelOldGc == null && options.containsKey("UseParallelOldGC")) {
                 analysis.add(Analysis.INFO_PARALLEL_OLD_CRUFT);
-                /*
-                 * boolean isParallelCollector = useDefaultCollector() && jvmContext.getVersionMajor() >= 7 &&
-                 * jvmContext.getVersionMajor() <= 8; if (!isParallelCollector && !JdkUtil.isOptionEnabled(useParNewGc))
-                 * { analysis.add(Analysis.INFO_PARALLEL_OLD_CRUFT); } else { if
-                 * (JdkUtil.isOptionDisabled(useParallelOldGc) && !JdkUtil.isOptionEnabled(useParNewGc)) {
-                 * analysis.add(Analysis.ERROR_PARALLEL_SCAVENGE_PARALLEL_SERIAL_OLD); } else if
-                 * (JdkUtil.isOptionEnabled(useParallelOldGc)) { analysis.add(Analysis.INFO_PARALLEL_OLD_REDUNDANT); } }
-                 */
             }
             // Check to see if explicit gc is disabled
             if (JdkUtil.isOptionEnabled(disableExplicitGc)) {
