2494c1646187c0acf5181c54ddccd0044febf41d
mmillson <mgm3746@users.noreply.github.com>
2023-03-03 08:38:34 -0500

    Add missing classes.

---
+++ src/main/java/org/github/joa/domain/Arch.java
@@ -0,0 +1,27 @@
+/**********************************************************************************************************************
+ * JVM Options Analyzer                                                                                               *
+ *                                                                                                                    *
+ * Copyright (c) 2022-2023 Mike Millson                                                                                    *
+ *                                                                                                                    *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License       *
+ * v. 2.0 which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is    *
+ * available at https://www.apache.org/licenses/LICENSE-2.0.                                                          *
+ *                                                                                                                    *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0                                                                     *
+ *                                                                                                                    *
+ * Contributors:                                                                                                      *
+ *    Mike Millson - initial API and implementation                                                                   *
+ *********************************************************************************************************************/
+package org.github.joa.domain;
+
+/**
+ * <p>
+ * Defined architectures.
+ * </p>
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ */
+public enum Arch {
+    //
+    I86PC, PPC64, PPC64LE, SPARC, UNKNOWN, X86, X86_64
+}
---
+++ src/main/java/org/github/joa/domain/BuiltBy.java
@@ -0,0 +1,47 @@
+/**********************************************************************************************************************
+ * JVM Options Analyzer                                                                                               *
+ *                                                                                                                    *
+ * Copyright (c) 2022-2023 Mike Millson                                                                                    *
+ *                                                                                                                    *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License       *
+ * v. 2.0 which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is    *
+ * available at https://www.apache.org/licenses/LICENSE-2.0.                                                          *
+ *                                                                                                                    *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0                                                                     *
+ *                                                                                                                    *
+ * Contributors:                                                                                                      *
+ *    Mike Millson - initial API and implementation                                                                   *
+ *********************************************************************************************************************/
+package org.github.joa.domain;
+
+/**
+ * <p>
+ * Defined builder strings.
+ * </p>
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ */
+public enum BuiltBy {
+    // Red Hat
+    BUILD,
+    // Graal
+    BUILDSLAVE,
+    // In some Oracle and Red Hat builds
+    EMPTY,
+    // Oracle: "Java Release Engineering"
+    JAVA_RE,
+    // AdoptOpenJDK
+    JENKINS,
+    // Oracle: "mach5one" is the name of their build/test system? Replaced by "java_re"?
+    MACH5ONE,
+    // Red Hat, CentOS
+    MOCKBUILD,
+    // Adoptium temurin
+    TEMURIN,
+    // Azul
+    TESTER, UNKNOWN,
+    // Microsoft
+    VSTS,
+    // Azul
+    ZULU_RE
+}
