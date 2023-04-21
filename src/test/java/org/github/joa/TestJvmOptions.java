3b383c6d791c9405bc73881726695c22be490ab7
mmillson <mgm3746@users.noreply.github.com>
2023-04-21 08:46:10 -0400

    Fix import.

--- src/test/java/org/github/joa/TestJvmOptions.java
+++ src/test/java/org/github/joa/TestJvmOptions.java
@@ -16,7 +16,6 @@ package org.github.joa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
